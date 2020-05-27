40ae81ba228adc7b85d5da8155b85850e7c42b74
benervous <45319856+benervous@users.noreply.github.com>
2020-05-27 19:38:36 +0300

    Update connect.php

--- API/connect.php
+++ API/connect.php
@@ -1,3 +1,3 @@
 <?php
-$connect = mysqli_connect('localhost', 'root', '', 'api_test');
+$connect = mysqli_connect('e7qyahb3d90mletd.chr7pe7iynqr.eu-west-1.rds.amazonaws.com', 'gqa9lyjfv5j4ik71', 'ag8i83vt16dju3zt', 'a4usv2ym5hpkdow8');
 ?>
